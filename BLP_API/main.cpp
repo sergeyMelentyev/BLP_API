136a16450702ee3f1f5afe8979009ac7c74f97b2
sergeyMelentyev <sergey.melentyev@gmail.com>
2016-05-13 22:24:54 +0300

    Initial Commit

---
+++ BLP_API/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BLP_API
+//
+//  Created by Olga on 13/05/16.
+//  Copyright © 2016 Sergey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
